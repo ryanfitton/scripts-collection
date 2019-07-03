2cd8bb0f812ec02ab80ad5498629e4a8569834cb
Ryan Fitton <ryanfitton@users.noreply.github.com>
2019-07-03 19:08:21 +0100

    Update delete-old-files-v2.sh

--- delete-old-files-v2.sh
+++ delete-old-files-v2.sh
@@ -6,19 +6,19 @@
 ##   chmod +x delete-old-files-v2.sh
 ##
 ## Pass arguments to the script:
-##   1: PATH		: The file path relative to /root (/) of the folder which will be checked for files. This is Recurive, any files which match the find query (path and age) within each sub-folder will be deleted.
+##   1: FOLDER		: The file path relative to /root (/) of the folder which will be checked for files. This is Recurive, any files which match the find query (folder and age) within each sub-folder will be deleted.
 ##   2: EXTENSION	: The file extension used by the find command, any files with this extension will be deleted.
 ##   3: AGE			: The age of files to be delated. Specify this in days, any files older than this value will be deleted.
 ##
 ## Run using:
-##   sudo ./delete-old-files-v2.sh '/your-path-here/' 'mp4' '30'
+##   sudo ./delete-old-files-v2.sh '/your-path-here/' '.mp4' '30'
 ##
 ## Notes: '/' = Server root folder. '~/' = User's home folder.
 
 
 # Deletion options
-PATH="$1"           		# The file path, this is recursive (files within sub-directories will be deleted)
-EXTENSION="$2"           	# The file extension used to target files
+FOLDER="$1"           		# The file path, this is recursive (files within sub-directories will be deleted)
+EXTENSION="$2"           	# The file extension used to target files, specify 'any' to target all
 AGE="$3"           			# (In days) If files are older than this value, they will be deleted
 
 
@@ -51,8 +51,19 @@ if [ "$1" != '' ] || [ "$2" != '' ] || [ "$3" != '' ];
 then
 
 
+	# Check if a specific extension should be used
+	if [ "$2" = 'any' ]; 
+
+	# If 'any' has been passed, dont search by extension
+	then
+		unset EXTENSION
+
+	fi
+
+
 	#Run the script
-	find $PATH -type f -name "*.$EXTENSION" -mtime +$AGE -exec rm {} +> /dev/null 2>&1
+	find $FOLDER -type f -name "*$EXTENSION" -mtime +$AGE -exec rm {} +> /dev/null 2>&1
+
 
 	echo "Deletion script has been run."
 	printf "\n"
